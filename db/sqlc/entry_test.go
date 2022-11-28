6f9fabdd651a93d661dc3aeeec67e34656842662
rzkyadhi <rizkyadhi081@gmail.com>
2022-11-28 20:16:42 +0700

    Creating entry_test and Adding Unit Test for CreateEntry

---
+++ db/sqlc/entry_test.go
@@ -0,0 +1,33 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/rzkyadhi/Simple-Bank/util"
+	"github.com/stretchr/testify/require"
+)
+
+func CreateRandomEntry(t *testing.T, account Account) Entry {
+	arg := CreateEntryParams{
+		AccountID: account.ID,
+		Amount:    util.RandomMoney(),
+	}
+
+	entry, err := testQueries.CreateEntry(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry)
+
+	require.Equal(t, arg.AccountID, entry.AccountID)
+	require.Equal(t, arg.Amount, entry.Amount)
+
+	require.NotZero(t, entry.ID)
+	require.NotZero(t, entry.CreatedAt)
+
+	return entry
+}
+
+func TestCreateEntry(t *testing.T) {
+	account := createRandomAccount(t)
+	CreateRandomEntry(t, account)
+}
