1415a4c19c6705820f66ac745abb16567774c428
rzkyadhi <rizkyadhi081@gmail.com>
2022-11-28 15:28:31 +0700

    Adding Unit Test for TestGetAccount

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -3,6 +3,7 @@ package db
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/rzkyadhi/Simple-Bank/util"
 	"github.com/stretchr/testify/require"
@@ -31,4 +32,18 @@ func createRandomAccount(t *testing.T) Account {
 
 func TestCreateAccount(t *testing.T) {
 	createRandomAccount(t)
+}
+
+func TestGetAccount(t *testing.T) {
+	accountTest := createRandomAccount(t)
+	accountGet, err := testQueries.GetAccount(context.Background(), accountTest.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, accountGet)
+
+	require.Equal(t, accountTest.ID, accountGet.ID)
+	require.Equal(t, accountTest.Owner, accountGet.Owner)
+	require.Equal(t, accountTest.Balance, accountGet.Balance)
+	require.Equal(t, accountTest.Currency, accountGet.Currency)
+	require.WithinDuration(t, accountTest.CreatedAt, accountGet.CreatedAt, time.Second)
 }
