a51afd92804745499be388b979bde279ddde19f0
rzkyadhi <rizkyadhi081@gmail.com>
2022-11-28 10:16:36 +0700

    Adding Unit Test for db connection and Implement Viper for Configuration

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+	"github.com/rzkyadhi/Simple-Bank/util"
+)
+
+var testQueries *Queries
+
+func TestMain(m *testing.M) {
+	config, err := util.LoadConfig("../../")
+	if err != nil {
+		log.Fatal("Cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("Cannot connect to db:", err)
+	}
+
+	testQueries = New(conn)
+
+	os.Exit(m.Run())
+}
